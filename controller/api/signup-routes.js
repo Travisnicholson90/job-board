a361d6877f394c474dc09161cd610cd7c8f43046
Voi Jankowski <wojtek.wacek.dola@gmail.com>
2023-05-17 14:40:07 +1000

    Installed bcryptjs and required it for the CREATE new user route.

--- controller/api/signup-routes.js
+++ controller/api/signup-routes.js
@@ -1,10 +1,11 @@
-const router = require('express').Router();
-const { User } = require('../../models');
+const router = require("express").Router();
+var bcrypt = require("bcryptjs");
+const { User } = require("../../models");
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     // Render the sign-up page
-    res.render('signup');
+    res.render("signup");
   } catch (err) {
     res.status(500).json(err);
   }
@@ -12,22 +13,26 @@ router.get('/', async (req, res) => {
 
 //create new user
 // does this need more?... e.g. what to do with data recieved, etc.
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     const { username, password } = req.body; //may have to change to first_name, last_name, suburb, email, password...
 
     const existingUser = await User.findOne({ where: { username } });
     if (existingUser) {
-      return res.status(400).json({message: 'Username already exists'});
+      return res.status(400).json({ message: "Username already exists" });
     }
 
     const newUser = await User.create({ username, password });
 
-    res.status(200).json({ message: 'Created user successfully!', user: newUser })
+    res
+      .status(200)
+      .json({ message: "Created user successfully!", user: newUser });
   } catch (err) {
-    if (err.name === 'SequelizeValidationError') {
+    if (err.name === "SequelizeValidationError") {
       const validationErrors = err.errors.map((error) => error.message);
-      return res.status(400).json({ message: 'Validation error', errors: validationErrors });
+      return res
+        .status(400)
+        .json({ message: "Validation error", errors: validationErrors });
     }
     res.status(500).json(err);
   }
