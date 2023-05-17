10c2dec043273e9f5e3694bbce968a11acebf1bc
Travis Nicholson <110001213+Travisnicholson90@users.noreply.github.com>
2023-05-18 08:25:22 +1000

    feature-landing-page

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const routes = require("./controller");
 const sequelize = require("./config/connection");
-const expressHbs = require("express-handlebars");
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-app.engine("hbs", expressHbs({ layoutsDir: "views/layouts/", defaultLayout: "main-layout", extname: "hbs" }));
-app.set("view engine", "handlebars");
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 app.set("views", "views");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
