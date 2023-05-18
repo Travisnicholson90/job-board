5aa09e288374aab267aa3f498efe40e535bc65ff
Voi Jankowski <wojtek.wacek.dola@gmail.com>
2023-05-18 10:36:02 +1000

    Changed GET categories route to fetch all.

--- controller/api/job-category-routes.js
+++ controller/api/job-category-routes.js
@@ -6,7 +6,7 @@ router.get("/", async (req, res) => {
   try {
     // res.render('categories');
     const categories = await JobCategory.findAll();
-    res.status(200).json(jobs);
+    res.status(200).json(categories);
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Cannot retrieve categories" });
