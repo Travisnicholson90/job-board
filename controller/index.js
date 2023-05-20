b2aa9557008a59841395f05d0457cbf6cd77f5f7
Voi Jankowski <wojtek.wacek.dola@gmail.com>
2023-05-20 15:39:08 +1000

    [200~added home route to index.js.

--- controller/index.js
+++ controller/index.js
@@ -1,10 +1,12 @@
-const router = require('express').Router();
-const apiRoutes = require('./api');
+const router = require("express").Router();
+const apiRoutes = require("./api");
+const homeRoutes = require("./home-routes");
 
-router.use('/api', apiRoutes);
+router.use("/api", apiRoutes);
+router.use("/", homeRoutes);
 
 router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
+  res.send("<h1>Wrong Route!</h1>");
 });
 
 module.exports = router;
