35bd53929bf2ca27b1daed6d87e3ce55e3aefb4d
Voi Jankowski <wojtek.wacek.dola@gmail.com>
2023-05-20 22:24:20 +1000

    Added connect-session-sequelize to server.js and set up the cookies for the length of a session and other cookies.

--- server.js
+++ server.js
@@ -2,17 +2,27 @@ const path = require("path");
 const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const routes = require("./controller");
 const sequelize = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set up sessions
+// Set up sessions and connect to Sequelize db
 const sess = {
   secret: "Super secret secret",
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000, // expires after 1 day
+    httpOnly: true,
+    secure: false,
+    sameSite: "strict",
+  },
   resave: false,
   saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess));
