2a2ecf01eb95ae3119a6970a91d68551fa307b5a
Voi Jankowski <wojtek.wacek.dola@gmail.com>
2023-05-21 15:56:17 +1000

    Added comments to deleteExpiredJobAdds with the referrences to sources of the methods.

--- cronjobs/deleteExpiredJobAds.js
+++ cronjobs/deleteExpiredJobAds.js
@@ -1,7 +1,9 @@
+// Used Crontab Guru: https://crontab.guru/every-night-at-midnight to generate schedule expression.
+// Used FreeCodeCamp: https://www.freecodecamp.org/news/schedule-a-job-in-node-with-nodecron/ to learn the methods of scheduling jobs.
+// Used Medium atricle: https://medium.com/@smallbee/make-a-sequelize-query-that-works-like-exact-in-operator-a64ee4817051 on using Sequelize operators in querries.
 const cron = require("node-cron");
 const { Job } = require("../models");
 const { Op } = require("sequelize");
-const moment = require("moment");
 
 // Delete expired job ads and job adds that are older than 1 month every day at 12:00am
 cron.schedule("0 0 * * *", async () => {
