ad3de1e3856feb5dbec35f5a20b3c202f142613d
Voi Jankowski <wojtek.wacek.dola@gmail.com>
2023-05-21 14:39:41 +1000

    Installed node-cron npm.

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const exphbs = require("express-handlebars");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const routes = require("./controller");
 const sequelize = require("./config/connection");
+const cron = require("node-cron");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
