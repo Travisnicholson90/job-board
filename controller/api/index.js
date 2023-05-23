27885d0ee9d417a61bafeaffc8230168920a76a3
TScar <tscarlett@live.com.au>
2023-05-23 19:40:56 +1000

    myjobs is almost there. just a bit buggy still

--- controller/api/index.js
+++ controller/api/index.js
@@ -4,12 +4,14 @@ const categoryRoutes = require('./job-category-routes.js');
 const listingRoutes = require('./job-routes.js');
 const signupRoutes = require('./signup-routes');
 const jobPostRoutes = require('./job-post-routes');
+const myJobRoutes = require('./myjobs-routes.js')
 
 router.use('/login', loginRoutes);
 router.use('/signup', signupRoutes);
 router.use('/categories', categoryRoutes);
 router.use('/jobs', listingRoutes);
 router.use('/job-post', jobPostRoutes);
+router.use('/myjobs', myJobRoutes);
 
 
 module.exports = router;
