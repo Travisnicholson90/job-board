16d49ac7268706dcaceccb43a48540b3d4bfa745
Travis Nicholson <110001213+Travisnicholson90@users.noreply.github.com>
2023-05-29 18:18:05 +1000

    final deployment

--- config/connection.js
+++ config/connection.js
@@ -10,9 +10,9 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: process.env.DB_HOST,
-      dialect: process.env.DB_DIALECT,
-      port: process.env.DB_PORT,
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
     }
   );
 }
